9b3189a8b3283742230ed7e7c28c4fa487bb6bd8
Niko Lindgren <77786248+bgm055NikoLindgren@users.noreply.github.com>
2021-03-16 13:05:36 +0200

    Add files via upload

---
+++ Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -0,0 +1,54 @@
+package com.example.Bookstore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.CategoryRepository;
+import com.example.Bookstore.domain.User;
+import com.example.Bookstore.domain.UserRepository;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
+		return (args) -> {
+
+			log.info("Save some test books");
+
+			crepository.save(new Category("Horror"));
+			crepository.save(new Category("Fantasy"));
+			crepository.save(new Category("Trains"));
+
+			repository.save(new Book("Eemeli", "Moi", 1990, "456123", crepository.findByName("Trains").get(0)));
+
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@gmail.com");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "admin@gmail.com");
+			urepository.save(user1);
+			urepository.save(user2);
+			
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+
+	}
+
+}
---
+++ Bookstore/src/main/java/com/example/Bookstore/domain/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.Bookstore.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findUserByUsername(String username);
+
+List <User> findByUsername (String username);
+}
